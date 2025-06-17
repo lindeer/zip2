7742665d777d9a85463a3094d1653f129e8a0073
lindeer <le.chang118@gmail.com>
2025-06-17 18:55:21 +0800

    example

--- example/example.dart
+++ example/example.dart
@@ -1,4 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:zip2/zip2.dart';
 
-void main() {
+void main() async {
+  final entries = Stream.fromIterable([
+    ZipFileEntry(
+      name: 'hello.txt',
+      data: Stream.fromIterable([
+        utf8.encode('Hello, Dart Zip Package!'),
+      ]),
+      lastModified: DateTime.now(),
+      method: ZipMethod.deflated, // DEFLATED
+    ),
+    ZipFileEntry(
+      name: 'empty.txt',
+      data: Stream.fromIterable([
+        Uint8List(0), // Empty file
+      ]),
+      lastModified: DateTime.now(),
+      method: ZipMethod.stored, // STORED (no compression)
+    ),
+  ]);
+  await entries
+      .transform(zip2.encoder)
+      .pipe(File('example.zip').openWrite());
 }
